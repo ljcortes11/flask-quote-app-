a69a4e36c00f98fe06254df7adb6ca3944cc5827
ljcortes11 <Luistaiga11@gmail.com>
2024-07-07 21:19:29 -0500

    new commit with flask

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, request, jsonify
-import random 
+import random
 
 app = Flask(__name__)
 
 quotes = [
     "The best way to predict the future is to invent it.",
-        "Life is 10% what happens to us and 10% what happens to us and 90% how we  react to it.",
-        "The only way to do great work is to love what you do.",
-        
+    "Life is 10% what happens to us and 90% how we react to it.",
+    "The only way to do great work is to love what you do."
 ]
 
 @app.route('/quote', methods=['GET'])
@@ -15,6 +14,6 @@ def get_quote():
     return jsonify({
         'quote': random.choice(quotes)
     })
-    
-    if __name__ == '__main__':
-        app.run(host='0.0.0.0',port=5000)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
